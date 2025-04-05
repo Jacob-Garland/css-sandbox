7dbb501b8b7927d0192bbb208d873ecd1218e3ce
Jacob Garland <352cjg@gmail.com>
2025-04-05 05:27:17 -0400

    various edits to header and app, gotta refactor context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { AppContextProvider } from "./context/AppContextProvider";
 import Header from "./components/Header";
 
 function App() {
 
   return (
     <ChakraProvider value={defaultSystem}>
-      <AppContextProvider>
-        <Header />
-      </AppContextProvider>
+      <Header />
     </ChakraProvider>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,47 @@
-import { Box, Heading, IconButton, NativeSelect } from '@chakra-ui/react';
-import { FaSun, FaMoon } from 'react-icons/fa';
-import { useContext, useState } from 'react';
+import { Box, Heading, IconButton, NativeSelect, HStack } from '@chakra-ui/react';
+import { LuSun, LuMoon } from 'react-icons/lu';
+import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import { Framework } from '../types';
 
 
 export default function Header() {
-    const { theme, toggleTheme } = useContext(AppContext);
-    const [value, setValue] = useState('');
+    const { framework, setFramework, theme, toggleTheme } = useContext(AppContext);
 
     return (
-        <Box position="fixed" top={0} left={0} right={0} p={4} bg="green.500" color="black">
-            <IconButton
-                as={theme === 'light' ? FaMoon : FaSun}
-                onClick={toggleTheme}
-                aria-label="Toggle theme"
-                variant={"ghost"}
-                mr={4}
-            />
-            <Heading as="h1" size="lg" textAlign="center" display="inline">CSS Sandbox</Heading>
-            <NativeSelect.Root width="320px" size="md">
-                <NativeSelect.Field placeholder="Select framework"
-                    value={value}
-                    onChange={(e) => setValue(e.currentTarget.value)}
-                >
-                    <option value="chakra">Chakra UI</option>
-                    <option value="bootstrap">Bootstrap</option>
-                    <option value="tailwind">Tailwind CSS</option>
-                    <option value="bulma">Bulma</option>
-                </NativeSelect.Field>
-            </NativeSelect.Root>
+        <Box position="fixed" top={0} left={0} right={0} py={4} bg="green.500" color="black" >
+            <HStack justifyContent="space-between" alignItems="center" px={4}>
+                <IconButton
+                    as={theme === 'light' ? LuMoon : LuSun}
+                    onClick={toggleTheme}
+                    aria-label="Toggle theme"
+                    variant={"ghost"}
+                    color={'black'}
+                    rounded={"full"}
+                    ml={7}
+                />
+                <Heading as="h1" size="lg" textAlign="center" fontStyle={"italic"} fontWeight={"bold"}>
+                    CSS Sandbox
+                </Heading>
+                <NativeSelect.Root width="240px" size="md" mr={7}>
+                    <NativeSelect.Field placeholder="Select framework"
+                        value={framework}
+                        onChange={(e) => setFramework(e.currentTarget.value as Framework)}
+                        color={'black'}
+                        bg={'gray.400'}
+                        fontWeight={"bold"}
+                        fontSize={"lg"}
+                        borderRadius={"md"}
+                        borderWidth={2}
+                        borderColor={theme === 'light' ? 'black' : 'white'}
+                    >
+                        <option value="chakra">Chakra UI</option>
+                        <option value="bootstrap">Bootstrap</option>
+                        <option value="tailwind">Tailwind CSS</option>
+                        <option value="bulma">Bulma</option>
+                    </NativeSelect.Field>
+                </NativeSelect.Root>
+            </HStack>
         </Box>
     );
 };
